ea077636194d415a6f2328d06baf6d6bd70d3ca1
DjaneBahar <baharsadik@hotmail.com>
2020-11-27 00:40:04 +0100

    oncklick with pictures

--- app/src/main/java/com/example/application/toolbarapp/MainActivity.java
+++ app/src/main/java/com/example/application/toolbarapp/MainActivity.java
@@ -26,16 +26,16 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_settings)
+                R.id.navigation_home, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+       // FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-        fragmentTransaction.add(R.id.container,new HomeFragment());
-        fragmentTransaction.commit();
+      //  fragmentTransaction.add(R.id.container,new HomeFragment());
+        //fragmentTransaction.commit();
 
         /*ExercisesFragment exercisesFragment = new ExercisesFragment();
         FragmentManager fm = getSupportFragmentManager();
